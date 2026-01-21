82875b2153a615efc67cde8c9b74a9e06ab31929
Hardik Raj <23052242@kiit.ac.in>
2026-01-21 10:23:46 +0530

    Add files via upload

---
+++ naive_bayes.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+df = pd.read_excel("simple_data.xls")
+X = df.drop("Target", axis=1)
+y = df["Target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Naive Bayes Accuracy:", accuracy_score(y_test, y_pred))
